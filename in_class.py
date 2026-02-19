730bbd81a0353eed81611bb70908c4d0c1c25711
Max Guryan <rhk2dx@virginia.edu>
2026-02-19 20:05:37 +0000

    in class work

--- in_class.py
+++ in_class.py
@@ -6,24 +6,25 @@ from sklearn.cluster import KMeans
 import matplotlib.pyplot as plt
 # %%
 # load data
-
-# %%
+df = pd.read_csv("house_votes_Dem.csv" , encoding = "latin-1") 
 # take a look at the data
-
+df.info()
 # %%
 # separate out the numeric features
-
+c_num = df[['aye' , 'nay' , 'other']]
 # %%
 # documentation for kmeans in sklearn
-
+help(KMeans)
 # %% build a kmeans model
-
-
+kmeans = KMeans(n_clusters = 3, random_state = 42 , verbose = 1)
+kmeans.fit(c_num)
 # %% look at the information in the model
-
+print(kmeans.cluster_centers_)
+print(kmeans.labels_)
 # %%
 # add the cluster labels to the original data frame
-
+df["cluster"] = kmeans.labels_
+df
 # %%
   
 # %% simple plot of the clusters
